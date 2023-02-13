e3ae4ff0b490d0083fb7df7daaba9df69bded509
Philipp Hug <philipp@hug.cx>
2023-02-13 13:20:09 +0100

    disabled module

--- lszt/odoo-addons-custom-mfgt/mfgt_remove_bank_account_constraint/__manifest__.py
+++ lszt/odoo-addons-custom-mfgt/mfgt_remove_bank_account_constraint/__manifest__.py
@@ -2,9 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'MFGT Remove Bank Account Constraint',
-    'version': '12.0.1.0',
-    'website' : 'http://syscoon.com',
+    'name' : 'MFGT Remove Bank Account Constraint (fake)',
+    'version': '16.0.1.0',
+    'website' : 'https://mfgt.ch',
     'category': 'Account',
     'description': """
 Removes Constraint for Bank Account on BVR-Payment

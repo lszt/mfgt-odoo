ae351c13d009faca8af94ae3ff42b41a224fca68
Philipp Hug <philipp@hug.cx>
2023-02-13 18:32:11 +0100

    fix depends

--- dummy/account_bank_statement_import_online/__manifest__.py
+++ dummy/account_bank_statement_import_online/__manifest__.py
@@ -5,8 +5,11 @@
     'version': '16.0.0.0',
     'website' : 'https://mfgt.ch',
     'category': 'Hidden/Dependency',
-    'depends' : [
-            ],
+    'depends': [
+        'account',
+        'account_bank_statement_import',
+        'web_widget_dropdown_dynamic',
+    ],
     'description': 'A dummy module for updating',
     'data': [
     ],

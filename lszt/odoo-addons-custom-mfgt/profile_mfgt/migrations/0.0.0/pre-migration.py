7c4e27805dd08cdea00979e8655de94ee43c95d8
Philipp Hug <philipp@hug.cx>
2023-03-01 14:11:20 +0100

    account_payment_sale

--- lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
+++ lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
@@ -1,2 +1,3 @@
 def migrate(cr, version):
     cr.execute("UPDATE public.ir_ui_view set active='f' where key like 'profile_mfgt.%'")
+    cr.execute("UPDATE public.ir_ui_view set active='f' where key like 'account_payment_sale.%'")

77a0511acf57c20dd67e10ef88545e471163a015
Philipp Hug <philipp@hug.cx>
2023-03-01 14:30:59 +0100

    disable view

--- lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
+++ lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
@@ -1,3 +1,4 @@
 def migrate(cr, version):
     cr.execute("UPDATE public.ir_ui_view set active='f' where key like 'profile_mfgt.%'")
     cr.execute("UPDATE public.ir_ui_view set active='f' where key like 'account_payment_sale.%'")
+    cr.execute("UPDATE public.ir_ui_view set active='f' where key like 'account_payment_partner.%")

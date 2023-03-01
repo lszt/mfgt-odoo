f15d5e8300788edde89ecc0be191ee7d734b3f27
Philipp Hug <philipp@hug.cx>
2023-03-01 11:23:10 +0100

    typo

--- lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
+++ lszt/odoo-addons-custom-mfgt/profile_mfgt/migrations/0.0.0/pre-migration.py
@@ -1,2 +1,2 @@
 def migrate(cr, version):
-    cr.execute('UPDATE public.ir_ui_view set active='f' where id=1035')
+    cr.execute('UPDATE public.ir_ui_view set active=`f` where key like `profile_mfgt.%`')
